941743d64811e684e4b0720749718a656cd83c32
sevdacode35 <119565000+mertcode35@users.noreply.github.com>
2023-03-20 11:19:13 +0300

    ..

--- src/app/pages/actions/list/table/columns/ImgCell.tsx
+++ src/app/pages/actions/list/table/columns/ImgCell.tsx
@@ -14,9 +14,9 @@ type Props = {
 const ImgCell: FC<Props> = ({item}) => (
   <div className='d-flex align-items-center'>
     <div className='d-flex flex-column'>
-      {item == undefined ?  '-' : <a target={'_blank'} href={`http://localhost:33755/${item}` } download >
+      {item == undefined ?  '-' : <a target={'_blank'} href={`https://freudapi.iqualitor.com/${item}` } download >
           <span className='text-gray-800'>
-            <img width='90' height= '80' src={`http://localhost:33755/${item}`} />
+            <img width='90' height= '80' src={`https://freudapi.iqualitor.com/${item}`} />
           </span>
           </a>}
     
