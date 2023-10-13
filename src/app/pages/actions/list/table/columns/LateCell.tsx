11d006bf36de6d332b85dd30048d8edc88a1d56c
Mert <mert@code35.net>
2023-10-13 13:51:26 +0300

    ....

--- src/app/pages/actions/list/table/columns/LateCell.tsx
+++ src/app/pages/actions/list/table/columns/LateCell.tsx
@@ -14,12 +14,20 @@ const LateCell: FC<Props> = ({item}) => {
   const intl = useIntl()
   const [isLate, setIsLate] = useState<any>(`${intl.formatMessage({id: 'No'})}`)
   //   console.log(item)
-
+  function parseDate(dateStr: any) {
+    const [day, month, year] = dateStr.split('.').map(Number)
+    return new Date(year, month - 1, day)
+  }
   useEffect(() => {
     const today = new Date() // Şu anki tarih ve saat
-    const lastDateTime = new Date(item?.lastDate).getTime() // lastDate tarih ve saati
-    const endDateTime = new Date(item?.endDate).getTime()
-    if (lastDateTime < endDateTime) {
+    const lastDateTime: any = parseDate(item?.lastDate) // lastDate tarih ve saati
+    const endDateTime: any = parseDate(item?.endDate)
+    console.log(item)
+    console.log(lastDateTime)
+    console.log(endDateTime)
+    if (isNaN(lastDateTime) || isNaN(endDateTime)) {
+      setIsLate(`${intl.formatMessage({id: '-'})}`)
+    } else if (lastDateTime < endDateTime) {
       setIsLate(`${intl.formatMessage({id: 'YES'})}`)
     } else setIsLate(`${intl.formatMessage({id: 'No'})}`)
   }, [])
